f6d221e59c6a4070ac5fe5177c341c57ccb74485
Agnieska125 <agnieska125@gmail.com>
2024-11-21 10:42:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('veikia');
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('input');
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log(navigator);
+
+    navigator.clipboard.writeText(input.value).then(() => {
+        btn.innerText = 'Copied';
+
+        setTimeout(() => {
+            btn.innerText = 'Copy'
+        }, 2000)
+    })
+
+})
